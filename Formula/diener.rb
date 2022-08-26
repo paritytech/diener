b7cae9a69a92038aaba7c4930307b32f83458188
CI <foo@example.invalid>
2022-08-26 12:12:11 +0000

    build: new homebrew formula for v0.4.4

--- Formula/diener.rb
+++ Formula/diener.rb
@@ -2,7 +2,7 @@ class Diener < Formula
   desc "dependency diener is a tool for easily changing Substrate or Polkadot dependency versions"
   homepage "https://github.com/paritytech/diener"
   url "https://github.com/paritytech/diener/releases/download/v0.4.4/diener_macos_v0.4.4.tar.gz"
-  sha256 "920fa14badc091a0cb7e89e79fb5ade3205f721b489b2f27640b5963bb670693"
+  sha256 "1caba305bcc460a528fbcf2f0a7dd519fb31ad21fffa9902d9497019809ff90b"
   version "0.4.4"
 
   def install
