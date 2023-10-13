40ae7fe687640875753f7a6107c874dd17474de4
NachoPal <ignacio.palacios.santos@gmail.com>
2023-10-13 10:33:34 +0200

    crates update working

--- src/update.rs
+++ src/update.rs
@@ -1,10 +1,11 @@
-use anyhow::{bail, ensure, Context, Result};
+use anyhow::{bail, ensure, Context, Result, anyhow};
 use git_url_parse::GitUrl;
 use std::{env::current_dir, fs, path::PathBuf, str::FromStr};
 use structopt::StructOpt;
-use toml_edit::{Document, InlineTable, Value};
+use toml_edit::{Document, InlineTable, Value, Item, Table};
 use walkdir::{DirEntry, WalkDir};
-
+use reqwest::header::USER_AGENT;
+use serde_json;
 /// Which dependencies should be rewritten?
 #[derive(Debug, Clone)]
 enum Rewrite {
@@ -15,12 +16,22 @@ enum Rewrite {
     Beefy(Option<String>),
 }
 
+/// The different sources `Version` can be generated from.
+#[derive(Debug, Clone)]
+enum VersionSource {
+    CratesIO,
+    Url(String),
+    File(String),
+}
+
+
 /// The version the dependencies should be switched to.
 #[derive(Debug, Clone)]
-enum Version {
+enum Key {
     Tag(String),
     Branch(String),
     Rev(String),
+    Version(VersionSource),
 }
 
 /// `update` subcommand options.
@@ -51,16 +62,20 @@ pub struct Update {
     all: bool,
 
     /// The `branch` that the dependencies should use.
-    #[structopt(long, conflicts_with_all = &[ "rev", "tag" ])]
+    #[structopt(long, conflicts_with_all = &[ "rev", "tag", "version" ])]
     branch: Option<String>,
 
     /// The `rev` that the dependencies should use.
-    #[structopt(long, conflicts_with_all = &[ "branch", "tag" ])]
+    #[structopt(long, conflicts_with_all = &[ "branch", "tag", "version" ])]
     rev: Option<String>,
 
     /// The `tag` that the dependencies should use.
-    #[structopt(long, conflicts_with_all = &[ "rev", "branch" ])]
+    #[structopt(long, conflicts_with_all = &[ "rev", "branch", "version" ])]
     tag: Option<String>,
+
+    /// The `version` that the dependencies should use.
+    #[structopt(long, conflicts_with_all = &[ "rev", "branch", "tag" ])]
+    version: Option<String>,
 
     /// Rewrite the `git` url to the give one.
     #[structopt(long)]
@@ -68,16 +83,19 @@ pub struct Update {
 }
 
 impl Update {
-    /// Convert the options into the parts `Rewrite`, `Version`, `Option<PathBuf>`.
-    fn into_parts(self) -> Result<(Rewrite, Version, Option<PathBuf>)> {
-        let version = if let Some(branch) = self.branch {
-            Version::Branch(branch)
+    /// Convert the options into the parts `Rewrite`, `Key`, `Option<PathBuf>`.
+    fn into_parts(self) -> Result<(Rewrite, Key, Option<PathBuf>)> {
+        let key = if let Some(branch) = self.branch {
+            Key::Branch(branch)
         } else if let Some(rev) = self.rev {
-            Version::Rev(rev)
+            Key::Rev(rev)
         } else if let Some(tag) = self.tag {
-            Version::Tag(tag)
+            Key::Tag(tag)
+        } else if let Some(version) = self.version {
+            let source = get_version_source(&version)?;
+            Key::Version(source)
         } else {
-            bail!("You need to pass `--branch`, `--tag` or `--rev`");
+            bail!("You need to pass `--branch`, `--tag`, `--rev` or `--version`.");
         };
 
         let rewrite = if self.all {
@@ -98,12 +116,12 @@ impl Update {
             bail!("You must specify one of `--substrate`, `--polkadot`, `--cumulus`, `--beefy` or `--all`.");
         };
 
-        Ok((rewrite, version, self.path))
+        Ok((rewrite, key, self.path))
     }
 
     /// Run this subcommand.
     pub fn run(self) -> Result<()> {
-        let (rewrite, version, path) = self.into_parts()?;
+        let (rewrite, key, path) = self.into_parts()?;
 
         let path = path
             .map(Ok)
@@ -130,59 +148,80 @@ impl Update {
             .filter(|e| {
                 e.file_type().is_file() && e.file_name().to_string_lossy().ends_with("Cargo.toml")
             })
-            .try_for_each(|toml| handle_toml_file(toml.into_path(), &rewrite, &version))
+            .try_for_each(|toml| handle_toml_file(toml.into_path(), &rewrite, &key))
     }
 }
 
 /// Handle a given dependency.
 ///
 /// This directly modifies the given `dep` in the requested way.
-fn handle_dependency(name: &str, dep: &mut InlineTable, rewrite: &Rewrite, version: &Version) {
-    let git = if let Some(git) = dep
-        .get("git")
-        .and_then(|v| v.as_str())
-        .and_then(|d| GitUrl::parse(d).ok())
-    {
-        git
-    } else {
-        return;
-    };
+fn handle_dependency(name: &str, dep: &mut InlineTable, rewrite: &Rewrite, key: &Key) -> Result<()> {
+    // let git = if let Some(git) = dep
+    //     .get("git")
+    //     .and_then(|v| v.as_str())
+    //     .and_then(|d| GitUrl::parse(d).ok())
+    // {
+    //     git
+    // } else {
+    //     return;
+    // };
 
-    let new_git = match rewrite {
-        Rewrite::All => &None,
-        Rewrite::Substrate(new_git) if git.name == "substrate" => new_git,
-        Rewrite::Polkadot(new_git) if git.name == "polkadot" => new_git,
-        Rewrite::Cumulus(new_git) if git.name == "cumulus" => new_git,
-        Rewrite::Beefy(new_git) if git.name == "grandpa-bridge-gadget" => new_git,
-        _ => return,
-    };
+    // let new_git = match rewrite {
+    //     Rewrite::All => &None,
+    //     Rewrite::Substrate(new_git) if git.name == "substrate" => new_git,
+    //     Rewrite::Polkadot(new_git) if git.name == "polkadot" => new_git,
+    //     Rewrite::Cumulus(new_git) if git.name == "cumulus" => new_git,
+    //     Rewrite::Beefy(new_git) if git.name == "grandpa-bridge-gadget" => new_git,
+    //     _ => return,
+    // };
 
-    if let Some(new_git) = new_git {
-        *dep.get_or_insert("git", "") = Value::from(new_git.as_str()).decorated(" ", "");
-    }
+    // if let Some(new_git) = new_git {
+    //     *dep.get_or_insert("git", "") = Value::from(new_git.as_str()).decorated(" ", "");
+    // }
+
+    // let package = if let Some(package) = dep.get("package").and_then(|v| v.as_str()) {
+    //   version
+    // } else {
+    //     return;
+    // };
 
     dep.remove("tag");
     dep.remove("branch");
     dep.remove("rev");
+    dep.remove("version");
+    dep.remove("path");
 
-    match version {
-        Version::Tag(tag) => {
+    match key {
+        Key::Tag(tag) => {
             *dep.get_or_insert("tag", "") = Value::from(tag.as_str()).decorated(" ", " ");
         }
-        Version::Branch(branch) => {
+        Key::Branch(branch) => {
             *dep.get_or_insert("branch", "") = Value::from(branch.as_str()).decorated(" ", " ");
         }
-        Version::Rev(rev) => {
+        Key::Rev(rev) => {
             *dep.get_or_insert("rev", "") = Value::from(rev.as_str()).decorated(" ", " ");
         }
+        Key::Version(source) => {
+            // *dep.get_or_insert("version", "") = Value::from(ver.as_str()).decorated(" ", " ");
+            let package = if let Some(package_name) = dep.get("package").and_then(|v| v.as_str()) {
+                package_name
+            } else {
+                name
+            };
+
+            let version = get_package_version(package, source)?;
+
+            *dep.get_or_insert("version", "") = Value::from(version.as_str()).decorated(" ", " ");
+        }
     }
-    log::debug!("  updated: {:?} <= {}", version, name);
+    log::debug!("  updated: {:?} <= {}", key, name);
+    Ok(())
 }
 
 /// Handle a given `Cargo.toml`.
 ///
 /// This means scanning all dependencies and rewrite the requested onces.
-fn handle_toml_file(path: PathBuf, rewrite: &Rewrite, version: &Version) -> Result<()> {
+fn handle_toml_file(path: PathBuf, rewrite: &Rewrite, key: &Key) -> Result<()> {
     log::info!("Processing: {}", path.display());
 
     let mut toml_doc = Document::from_str(&fs::read_to_string(&path)?)?;
@@ -203,10 +242,82 @@ fn handle_toml_file(path: PathBuf, rewrite: &Rewrite, version: &Version) -> Resu
                     let table = toml_doc[k][dn]
                         .as_inline_table_mut()
                         .expect("We filter by `is_inline_table`; qed");
-                    handle_dependency(dn, table, rewrite, version);
+                    let a = handle_dependency(dn, table, rewrite, key);
+                    // handle_dependency(dn, table, rewrite, key);
+
+                    log::debug!("Result {:?}", a);
                 })
         });
 
     fs::write(&path, toml_doc.to_string())?;
     Ok(())
+}
+
+fn get_version_source(version: &String) -> Result<VersionSource> {
+    let source = if version.starts_with("http://") || version.starts_with("https://") {
+        VersionSource::Url(version.clone())
+    } else {
+        let path = PathBuf::from(version);
+        if path.exists() && path.file_name() == Some("Cargo.lock".as_ref()) {
+            VersionSource::File(version.clone())
+        } else if version == "latest" {
+            VersionSource::CratesIO
+        } else {
+            bail!("Invalid 'version' source: {}", version)
+        }
+    };
+    Ok(source)
+}
+
+fn get_package_version(package: &str, source: &VersionSource) -> Result<String> {
+    let version = match source {
+        VersionSource::CratesIO => {
+            let url = format!("https://crates.io/api/v1/crates/{}", package);
+            log::debug!("URL {:?}", url);
+
+            // let body = reqwest::blocking::get(&url)?.text()?;
+            let client = reqwest::blocking::Client::new();
+            let body = client.get(&url)
+                .header(USER_AGENT, "test_crawler_diener")
+                .send()?.text()?;
+            log::debug!("BODY {:?}", body);
+            let json: serde_json::Value = serde_json::from_str(&body).map_err(|_| {
+                anyhow::anyhow!("Error trying to JSON parse the crates.io response: {}", body)
+            })?;
+            log::debug!("JSON {:?}", json);
+            json["crate"]["max_version"].as_str().ok_or(
+                anyhow::anyhow!("Package '{}' not found on crates.io", package)
+            )?.to_string()
+        }
+        VersionSource::Url(url) => {
+            let body = reqwest::blocking::get(url)?.text()?;
+            get_package_version_from_cargo_lock_file(body, package).ok_or(
+                anyhow::anyhow!("Package '{}' not found in Cargo.lock", package)
+            )?
+        }
+        VersionSource::File(path) => {
+            let path = PathBuf::from(path);
+            let body = fs::read_to_string(path)?;
+            get_package_version_from_cargo_lock_file(body, package).ok_or(
+                anyhow::anyhow!("Package '{}' not found in Cargo.lock", package)
+            )?
+        }
+    };
+    Ok(version)
+}
+
+fn get_package_version_from_cargo_lock_file(body: String, package_name: &str) -> Option<String> {
+    let doc = body.parse::<Document>().ok()?;
+    let package_table = doc["package"].as_array_of_tables()?;
+
+    for package in package_table.iter() {
+        if let Some(name) = package["name"].as_str() {
+            if name == package_name {
+                if let Some(version) = package["version"].as_str() {
+                    return Some(version.to_string());
+                }
+            }
+        }
+    }
+    None
 }
